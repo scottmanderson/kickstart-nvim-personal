e7342cf54a5f17e283a6aa30f2bb198525e35e36
scott <Scott Anderson 30956117+scottmanderson@users.noreply.github.com>
2024-02-04 14:06:00 -0600

    added markdown previewer

---
+++ lua/custom/plugins/markdown-preview.lua
@@ -0,0 +1,8 @@
+-- https://github.com/iamcco/markdown-preview.nvim
+
+return {
+  "iamcco/markdown-preview.nvim",
+  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+    ft = { "markdown" },
+    build = function() vim.fn["mkdp#util#install"]() end,
+}
