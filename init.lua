d50753f7110311d01251617cc2503d9cbc652950
scott <Scott Anderson 30956117+scottmanderson@users.noreply.github.com>
2024-06-22 15:22:26 -0500

    added todo-comments, trouble diag plugins

--- init.lua
+++ init.lua
@@ -153,16 +153,34 @@ require('lazy').setup({
       end,
     },
   },
-
+  --[[
   {
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
-    priority = 1000,
+    priority = 800,
     config = function()
       vim.cmd.colorscheme 'onedark'
     end,
   },
+  --]]
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
+    'folke/tokyonight.nvim',
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 900, -- make sure to load this before all the other start plugins
+    config = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight-night'
 
+      -- You can configure highlights by doing something like
+      vim.cmd.hi 'Comment gui=none'
+    end,
+  },
   {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
@@ -170,7 +188,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'tokyonight',
         component_separators = '|',
         section_separators = '',
       },
@@ -638,4 +656,14 @@ vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Plugin Mappings
 vim.keymap.set("n", "<leader>tt", "<CMD>Neotree toggle<CR>")
+
+-- Split Configuration
+vim.o.splitright = true
+vim.o.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
 
