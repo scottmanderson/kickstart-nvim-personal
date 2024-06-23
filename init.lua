b58019100c3f14f9b8a2ddf14c80dfca9a9a0c38
scott <Scott Anderson 30956117+scottmanderson@users.noreply.github.com>
2024-06-22 22:59:15 -0500

    Python virtual environment switching, refactoring keybindings

--- init.lua
+++ init.lua
@@ -677,7 +677,23 @@ vim.keymap.set("n", "<leader>w", "<C-W>")
 vim.keymap.set("i", "<C-c>", "<Esc>")
 
 -- Plugin Mappings
+--Neotree
 vim.keymap.set("n", "<leader>tt", "<CMD>Neotree toggle<CR>")
+
+--Refactoring
+vim.keymap.set("x", "<leader>re", ":Refactor extract ")
+vim.keymap.set("x", "<leader>rf", ":Refactor extract_to_file ")
+vim.keymap.set("x", "<leader>rv", ":Refactor extract_var ")
+vim.keymap.set({ "n", "x" }, "<leader>ri", ":Refactor inline_var")
+vim.keymap.set( "n", "<leader>rI", ":Refactor inline_func")
+vim.keymap.set("n", "<leader>rb", ":Refactor extract_block")
+vim.keymap.set("n", "<leader>rbf", ":Refactor extract_block_to_file")
+
+--swenv python environment selection
+vim.keymap.set("n", "<leader>pv", function ()
+  require("swenv.api").set_venv(".venv")
+  end, { desc = "Python Virtual Environment .venv Select" })
+
 
 -- Split Configuration
 vim.o.splitright = true
